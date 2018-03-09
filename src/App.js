85539b4588b24a57b3d640095986c04216f37008
Jin <wangatqueens@gmail.com>
2018-03-08 23:12:19 -0500

    lightning update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, {
   Component
 } from 'react';
-
+import lightning from './lightning.png';
 import './App.css';
 
 
 class LighteningCounter extends Component {
   constructor(props) {
     super(props);
-    //set initial states here
+    //ES6: set initial states here
     this.state = {
-      strikes: 100
+      strikes: 0
     };
     this.timerTick = this.timerTick.bind(this);
   }
@@ -40,6 +40,25 @@ class LighteningCounter extends Component {
 }
 
 class LighteningCounterDisplay extends Component {
+  constructor(props) {
+    super(props);
+    //set initial states here
+    this.state = {
+      left: 40,
+      top: 40
+    };
+    this.lightningShift = this.lightningShift.bind(this);
+  }
+
+  lightningShift() {
+    this.setState({
+      left: Math.random() * 280,
+      top: Math.random() * 280
+    });
+  }
+  componentDidMount() {
+    setInterval(this.lightningShift, 2000);
+  }
   render() {
     let commonStyle = {
       margin: 0,
@@ -68,23 +87,35 @@ class LighteningCounterDisplay extends Component {
         ...commonStyle
       }
     }
+    let lightningStyle = {
+      width: 48,
+      height: 48,
+      position: "absolute",
+      left: this.state.left,
+      top: this.state.top
+    }
     return (
       <div style={disStyle}>
-        <LighteningCounter />
-        <h2 style={textStyle.smallEmphasis}>
-          Lightning strikes on
-        </h2>
+          
+          <div style={lightningStyle}>
+            <img src={lightning} alt="lightning" />
+          </div>
+          <LighteningCounter />
+          <h2 style={textStyle.smallEmphasis}>
+            Lightning strikes on
+          </h2>
+          
+          <h2 style={textStyle.emphasis}>
+            Our Earth
+          </h2>
 
-        <h2 style={textStyle.emphasis}>
-          Our Earth
-        </h2>
-
-        <p style={textStyle.smallEmphasis}>
-          (from the component was loaded)
-        </p>
-        <p style={textStyle.smallEmphasis}>
-          see <a href="https://www.lightningmaps.org">real-time Lightning Map</a>
-        </p>
+          <p style={textStyle.smallEmphasis}>
+            (from the component was loaded)
+          </p>
+          <p style={textStyle.smallEmphasis}>
+            see <a href="https://www.lightningmaps.org">real-time Lightning Map</a>
+          </p>
+          
       </div>
     );
   }
